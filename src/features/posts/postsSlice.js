13274dc197e2fff7eac6281f5f845e71048542af
Bryan Guevara <bryan19052000@gmail.com>
2022-11-18 23:58:15 -0800

    TODO: Add emoji reactions to posts

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,56 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
+
+const initialState = [
+  {
+    id: '1',
+    title: 'First Post!',
+    content: 'Hello',
+    user: '0',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+  },
+  {
+    id: '2',
+    title: 'Second Post',
+    content: 'More text',
+    user: '1',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+  },
+]
+
+const postSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            date: new Date().toISOString(),
+            title,
+            content,
+            user: userId,
+          },
+        }
+      },
+    },
+
+    postUpdated(state, action) {
+      const { id, title, content } = action.payload
+      const existingPost = state.find((post) => post.id === id)
+
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
+    },
+  },
+})
+
+export const { postAdded, postUpdated } = postSlice.actions
+
+export default postSlice.reducer
